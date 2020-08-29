d5844cb2a5d324f795ae104003e6bfa8f301389a
Jack Steel <jsteel@google.com>
2020-08-29 17:23:57 +1000

    Add hello world HTTP route

--- valorantcompetitivebot/webserver/main.py
+++ valorantcompetitivebot/webserver/main.py
@@ -3,7 +3,16 @@ from aiohttp import web
 
 async def start():
     app = web.Application()
+    app.add_routes(routes)
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, host='127.0.0.1', port=8080)
+    site = web.TCPSite(runner, host='0.0.0.0', port=8080)
     await site.start()
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def hello(request):
+    return web.Response(text="Hello, World!")
